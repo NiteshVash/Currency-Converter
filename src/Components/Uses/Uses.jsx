dd331e18c372f94cee0adfede5b818d02c4ad814
Nitesh Vashist <137484277+NiteshVash@users.noreply.github.com>
2025-03-09 22:16:00 +0530

    .....

--- src/Components/Uses/Uses.jsx
+++ src/Components/Uses/Uses.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import './Uses.css'
+import React from 'react';
+import './Uses.css';
 
-const Uses = () => {
+function Uses() {
   return (
     <div className="uses-container">
-        <h2>How to Use the Currency Converter</h2>
-        <ol>
-            <li>Select the currency you want to convert from in the first dropdown menu.</li>
-            <li>Select the currency you want to convert to in the second dropdown menu.</li>
-            <li>Enter the amount you want to convert in the input field.</li>
-            <li>Click the "Convert" button to see the converted amount.</li>
-        </ol>
+      <h2>How to Use the Currency Converter</h2>
+      <ol>
+        <li>Select the currency you want to convert from in the first dropdown menu.</li>
+        <li>Select the currency you want to convert to in the second dropdown menu.</li>
+        <li>Enter the amount you want to convert in the input field.</li>
+        <li>Click the "Convert" button to see the converted amount.</li>
+      </ol>
     </div>
-  )
+  );
 }
 
-export default Uses
+export default Uses;
